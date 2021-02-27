235a8ea566715fae838e2f74bf0a8db3a50b9c01
sachin2220sk <78066827+sachin2220sk@users.noreply.github.com>
2021-02-27 13:00:03 +0530

    Add files via upload

--- vsdflow.tcl
+++ vsdflow.tcl
@@ -335,7 +335,6 @@ puts "\nInfo: SDC created. Please use constraints in path  $OutputDirectory/$Des
 }
 
 
-
 #----------------------------------------------------------------------------#
 #-------------Hierarchy check and synthesis using yosys from qflow-----------#
 #----------------------------------------------------------------------------#
@@ -383,31 +382,9 @@ if { [catch { exec yosys -s $OutputDirectory/$DesignName.hier.ys >& $OutputDirec
 puts "\nInfo:Please fine hierarchy check details in [file normalize $OutputDirectory/$DesignName.hierarchy_check.log] for more information"
 cd $working_dir
 
-
-
 #-----------------------------------------------------------------------------#
 #--------------------------- Main Synthesis Script ---------------------------#
 #-----------------------------------------------------------------------------#
-
-
-file mkdir $OutputDirectory/$DesignName/src
-file copy $NetlistDirectory/$DesignName.v $OutputDirectory/$DesignName/src
-file copy $OutputDirectory/$DesignName.sdc $OutputDirectory/$DesignName/src
-file copy $OutputDirectory/$DesignName $working_dir/work/tools/openlane_working_dir/openlane/designs
-
-
-cd $working_dir/work/tools/openlane_working_dir/openlane
-
-export PDK_ROOT=/$working_dir/home/work/tools/openlane_working_dir/pdks
-alias docker run -it -v $(pwd):/openLANE_flow -v $PDK_ROOT:$PDK_ROOT -e PDK_ROOT=$PDK_ROOT -u $(id -u $USER):$(id -g $USER) openlane:rc6
-./flow.tcl -design $DesignName -init_design_config
-./flow.tcl -design $DesignName
-
-
-
-return
-
-
 
 puts "\nInfo: Creating main synthesis script to be used by yosys"
 set data "read_liberty -lib -ignore_miss_dir -setattr blackbox ${LateLibraryPath}"
@@ -447,9 +424,167 @@ file mkdir $OutputDirectory/layout
 #file link -symbolic $OutputDirectory/$DesignName.synth.v $OutputDirectory/synthesis/$DesignName.rtlnopwr.v
 
 #---------------------------------------------------------------------#
-#---------------------- Run Synthesis using Yosys -------------#
+#---------------------- Run Synthesis Script using Yosys -------------#
 #---------------------------------------------------------------------#
 
+set ::env(OPENLANE_ROOT) [file dirname [file normalize [info script]]]
+
+lappend ::auto_path "$::env(OPENLANE_ROOT)/scripts/"
+package require openlane; # provides the utils as well
+
+proc run_non_interactive_mode {args} {
+        set options {
+                {-design required}
+                {-save_path optional}
+        }
+        set flags {-save}
+        parse_key_args "run_non_interactive_mode" args arg_values $options flags_map $flags -no_consume
+
+        prep {*}$args
+
+        run_synthesis
+        run_floorplan
+        run_placement
+        run_cts
+        run_routing
+
+        if { ($::env(DIODE_INSERTION_STRATEGY) == 2) || ($::env(DIODE_INSERTION_STRATEGY) == 5) } {
+                run_antenna_check
+                heal_antenna_violators; # modifies the routed DEF
+        }
+    }
+
+        run_magic
+
+        run_magic_spice_export
+
+        if {  [info exists flags_map(-save) ] } {
+                if { ! [info exists arg_values(-save_path)] } {
+                        set arg_values(-save_path) ""
+                }
+                save_views      -lef_path $::env(magic_result_file_tag).lef \
+                        -def_path $::env(tritonRoute_result_file_tag).def \
+                        -gds_path $::env(magic_result_file_tag).gds \
+                        -mag_path $::env(magic_result_file_tag).mag \
+                        -maglef_path $::env(magic_result_file_tag).lef.mag \
+                        -spice_path $::env(magic_result_file_tag).spice \
+                        -verilog_path $::env(CURRENT_NETLIST) \
+                        -save_path $arg_values(-save_path) \
+                        -tag $::env(RUN_TAG)
+        }
+# Physical verification
+
+        run_magic_drc
+
+        run_lvs; # requires run_magic_spice_export
+
+        run_antenna_check
+
+        run_lef_cvc
+
+        generate_final_summary_report
+
+        puts_success "Flow Completed Without Fatal Errors."
+}
+
+proc run_interactive_mode {args} {
+        set options {
+                {-design optional}
+        }
+        set flags {}
+        parse_key_args "run_interactive_mode" args arg_values $options flags_map $flags -no_consume
+
+        if { [info exists arg_values(-design)] } {
+                prep {*}$args
+        }
+
+        set ::env(TCLLIBPATH) $::auto_path
+        exec tclsh >&@stdout
+}
+roc run_magic_drc_batch {args} {
+        set options {
+                {-magicrc optional}
+                {-tech optional}
+                {-report required}
+                {-design required}
+                {-gds required}
+        }
+        set flags {}
+        parse_key_args "run_magic_drc_batch" args arg_values $options flags_mag $flags
+        if { [info exists arg_values(-magicrc)] } {
+                set magicrc [file normalize $arg_values(-magicrc)]
+        }
+        if { [info exists arg_values(-tech)] } {
+                set ::env(TECH) [file normalize $arg_values(-tech)]
+        }
+        set ::env(GDS_INPUT) [file normalize $arg_values(-gds)]
+        set ::env(REPORT_OUTPUT) [file normalize $arg_values(-report)]
+        set ::env(DESIGN_NAME) $arg_values(-design)
+if { [info exists magicrc] } {
+                exec magic \
+                        -noconsole \
+                        -dnull \
+                        -rcfile $magicrc \
+                        $::env(OPENLANE_ROOT)/scripts/magic/drc_batch.tcl \
+                        </dev/null |& tee /dev/tty
+        } else {
+                exec magic \
+                        -noconsole \
+                        -dnull \
+                        $::env(OPENLANE_ROOT)/scripts/magic/drc_batch.tcl \
+                        </dev/null |& /dev/tty
+        }
+}
+
+proc run_file {args} {
+        set ::env(TCLLIBPATH) $::auto_path
+        exec tclsh {*}$args >&@stdout
+}
+
+set options {
+        {-file optional}
+}
+
+set flags {-interactive -it -drc -synth_explore}
+arse_key_args "flow.tcl" argv arg_values $options flags_map $flags -no_consume
+
+puts_info {
+        ___   ____   ___  ____   _       ____  ____     ___
+        /   \ |    \ /  _]|    \ | |     /    ||    \   /  _]
+        |     ||  o  )  [_ |  _  || |    |  o  ||  _  | /  [_
+        |  O  ||   _/    _]|  |  || |___ |     ||  |  ||    _]
+        |     ||  | |   [_ |  |  ||     ||  _  ||  |  ||   [_
+        \___/ |__| |_____||__|__||_____||__|__||__|__||_____|
+
+}
+if {[catch {exec git --git-dir $::env(OPENLANE_ROOT)/.git describe --tags} ::env(OPENLANE_VERSION)]} {
+        # if no tags yet
+        if {[catch {exec git --git-dir $::env(OPENLANE_ROOT)/.git log --pretty=format:'%h' -n 1} ::env(OPENLANE_VERSION)]} {
+                set ::env(OPENLANE_VERSION) "N/A"
+        }
+}
+
+puts_info "Version: $::env(OPENLANE_VERSION)"
+
+if { [info exists flags_map(-interactive)] || [info exists flags_map(-it)] } {
+        puts_info "Running interactively"
+        if { [info exists arg_values(-file)] } {
+                run_file [file normalize $arg_values(-file)] {*}$argv
+        } else {
+                run_interactive_mode {*}$argv
+        }
+} elseif { [info exists flags_map(-drc)] } {
+        run_magic_drc_batch {*}$argv
+} elseif { [info exists flags_map(-synth_explore)] } {
+        prep {*}$argv
+        run_synth_exploration
+} else {
+        puts_info "Running non-interactively"
+        run_non_interactive_mode {*}$argv
+}
+
+
+return
 
 if {[catch { exec yosys -s $OutputDirectory/$DesignName.ys >& $OutputDirectory/$DesignName.synthesis.log} msg]} {
 	puts "\nError: Synthesis failed due to errors . Refer to log $OutputDirectory/$DesignName.synthesis.log for errors"
