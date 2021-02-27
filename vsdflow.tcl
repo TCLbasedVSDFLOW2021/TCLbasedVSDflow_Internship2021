9ec165b2d3b9b707245239091edf9fe268b94874
sachin2220sk <78066827+sachin2220sk@users.noreply.github.com>
2021-02-27 10:57:37 +0530

    Update vsdflow.tcl

--- vsdflow.tcl
+++ vsdflow.tcl
@@ -399,7 +399,7 @@ file copy $OutputDirectory/$DesignName $working_dir/work/tools/openlane_working_
 cd $working_dir/work/tools/openlane_working_dir/openlane
 
 export PDK_ROOT=/$working_dir/home/work/tools/openlane_working_dir/pdks
-docker run -it -v $(pwd):/openLANE_flow -v $PDK_ROOT:$PDK_ROOT -e PDK_ROOT=$PDK_ROOT -u $(id -u $USER):$(id -g $USER) openlane:rc6
+alias docker run -it -v $(pwd):/openLANE_flow -v $PDK_ROOT:$PDK_ROOT -e PDK_ROOT=$PDK_ROOT -u $(id -u $USER):$(id -g $USER) openlane:rc6
 ./flow.tcl -design $DesignName -init_design_config
 ./flow.tcl -design $DesignName
 
